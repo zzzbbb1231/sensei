5e1d142dd7c8ccbe634f6a87fbfab584e3d7b5bf
Dhaval Patel <dpatel@linkedin.com>
2012-10-07 15:54:12 -0700

    adding the ability to create a segment on hadoop

--- sensei-ba/gazelle/src/main/java/com/senseidb/ba/gazelle/creators/SegmentCreator.java
+++ sensei-ba/gazelle/src/main/java/com/senseidb/ba/gazelle/creators/SegmentCreator.java
@@ -19,6 +19,9 @@ import org.apache.avro.util.Utf8;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.log4j.Logger;
 
 import com.browseengine.bobo.facets.data.TermValueList;
@@ -32,128 +35,274 @@ import com.senseidb.ba.gazelle.utils.CompressedIntArray;
 public class SegmentCreator {
   private static Logger logger = Logger.getLogger(SegmentCreator.class);
 
-  public static GazelleIndexSegmentImpl readFromAvroFile(File avroFile) throws IOException {
+  public static GazelleIndexSegmentImpl readFromAvroFileOnHadoop(String avroFilePath, FileSystem fs) throws IOException {
     MetadataCreator creator = new MetadataCreator();
-    FileInputStream inputStream1 = null;
-    FileInputStream inputStream2 = null;
+    Path path = new Path(avroFilePath);
+    FSDataInputStream inputStream1 = null;
+    FSDataInputStream inputStream2 = null;
     try {
-    Map<String, GazelleForwardIndexImpl> ret = new HashMap<String, GazelleForwardIndexImpl>();
-  DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
-    inputStream1 = new FileInputStream(avroFile);
+      Map<String, GazelleForwardIndexImpl> ret = new HashMap<String, GazelleForwardIndexImpl>();
+      DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
+      inputStream1 = fs.open(path);
       DataFileStream<GenericRecord> dataFileReader = new DataFileStream<GenericRecord>(inputStream1, datumReader);
 
-    Schema schema = dataFileReader.getSchema();
-  if (dataFileReader.getSchema() == null) {
-      throw new IllegalStateException();
-    }
-    ColumnType[] columnTypes = new ColumnType[schema.getFields().size()];
-    String[] columnNames = new String[schema.getFields().size()];
-    int i = 0;
-    for (Field field : schema.getFields()) {
+      Schema schema = dataFileReader.getSchema();
+      if (dataFileReader.getSchema() == null) {
+        throw new IllegalStateException();
+      }
+      ColumnType[] columnTypes = new ColumnType[schema.getFields().size()];
+      String[] columnNames = new String[schema.getFields().size()];
+      int i = 0;
+      for (Field field : schema.getFields()) {
         Type type = field.schema().getType();
         if (type == Type.UNION) {
-            type = ((Schema) CollectionUtils.find(field.schema().getTypes(), new Predicate() {
-                  @Override
-                  public boolean evaluate(Object object) {
-                      return ((Schema) object).getType() != Type.NULL;
-                  }
-              })).getType();
+          type = ((Schema) CollectionUtils.find(field.schema().getTypes(), new Predicate() {
+            @Override
+            public boolean evaluate(Object object) {
+              return ((Schema) object).getType() != Type.NULL;
+            }
+          })).getType();
         }
-          ColumnType columnType = ColumnType.valueOf(type);
-      columnTypes[i] = columnType;
+        ColumnType columnType = ColumnType.valueOf(type);
+        columnTypes[i] = columnType;
         columnNames[i] = field.name();
-      i++;
-    }
-    DictionaryCreator[] creators = new DictionaryCreator[schema.getFields().size()];
-    for (int j = 0; j < columnTypes.length; j++) {
-      creators[j] = new DictionaryCreator();
-  }
-    int count = 0;
-    Iterator<GenericRecord> iterator = dataFileReader.iterator();
-    while (iterator.hasNext()) {
+        i++;
+      }
+      DictionaryCreator[] creators = new DictionaryCreator[schema.getFields().size()];
+      for (int j = 0; j < columnTypes.length; j++) {
+        creators[j] = new DictionaryCreator();
+      }
+      int count = 0;
+      Iterator<GenericRecord> iterator = dataFileReader.iterator();
+      while (iterator.hasNext()) {
         GenericRecord record = iterator.next();
         for (int j = 0; j < columnTypes.length; j++) {
 
           Object value = record.get(j);
           if (value instanceof Utf8) {
-          value = ((Utf8) value).toString();
-      }
-              creators[j].addValue(value, columnTypes[j]);
-    }
-        /*if (count % 100000 == 0) {
-          System.out.println("!count = " + count);
-        } */
+            value = ((Utf8) value).toString();
+          }
+          creators[j].addValue(value, columnTypes[j]);
+        }
+        /*
+         * if (count % 100000 == 0) { System.out.println("!count = " + count); }
+         */
         count++;
-  }
-    logger.info("Created dictionaries for " + count + " elements from the avro file");
-    TermValueList[] dictionaries = new TermValueList[schema.getFields().size()];
-    for (int j = 0; j < columnTypes.length; j++) {
-      dictionaries[j] = creators[j].produceDictionary();
-  }
-    CompressedIntArray[] intArrays = new CompressedIntArray[schema.getFields().size()];
-    SortedForwardIndexImpl[] sortedForwardIndexes = new SortedForwardIndexImpl[schema.getFields().size()];
-    for (int j = 0; j < columnTypes.length; j++) {
-      if (!creators[j].isSorted()) {
-        //System.out.println(columnNames[j] + " = " + dictionaries[j].size());  
-        intArrays[j] = new CompressedIntArray(count, CompressedIntArray.getNumOfBits(dictionaries[j].size()), getByteBuffer(count, dictionaries[j].size()));
-      } else {
-          sortedForwardIndexes[j] = new SortedForwardIndexImpl(dictionaries[j], new int[dictionaries[j].size()], new int[dictionaries[j].size()], count, creator.createMetadata(columnNames[j], dictionaries[j], columnTypes[j], count,  true));
       }
-    }
-    dataFileReader.close();
-    datumReader = new GenericDatumReader<GenericRecord>();
-    inputStream2 = new FileInputStream(avroFile);
-      
-    dataFileReader = new DataFileStream<GenericRecord>(inputStream2, datumReader);
-     
-    iterator = dataFileReader.iterator();
-    i = 0;
-    while (dataFileReader.hasNext()) {
+      logger.info("Created dictionaries for " + count + " elements from the avro file");
+      TermValueList[] dictionaries = new TermValueList[schema.getFields().size()];
+      for (int j = 0; j < columnTypes.length; j++) {
+        dictionaries[j] = creators[j].produceDictionary();
+      }
+      CompressedIntArray[] intArrays = new CompressedIntArray[schema.getFields().size()];
+      SortedForwardIndexImpl[] sortedForwardIndexes = new SortedForwardIndexImpl[schema.getFields().size()];
+      for (int j = 0; j < columnTypes.length; j++) {
+        if (!creators[j].isSorted()) {
+          // System.out.println(columnNames[j] + " = " +
+          // dictionaries[j].size());
+          intArrays[j] =
+              new CompressedIntArray(count, CompressedIntArray.getNumOfBits(dictionaries[j].size()), getByteBuffer(
+                  count, dictionaries[j].size()));
+        } else {
+          sortedForwardIndexes[j] =
+              new SortedForwardIndexImpl(dictionaries[j], new int[dictionaries[j].size()],
+                  new int[dictionaries[j].size()], count, creator.createMetadata(columnNames[j], dictionaries[j],
+                      columnTypes[j], count, true));
+        }
+      }
+      dataFileReader.close();
+      datumReader = new GenericDatumReader<GenericRecord>();
+      inputStream2 = fs.open(path);
+
+      dataFileReader = new DataFileStream<GenericRecord>(inputStream2, datumReader);
+
+      iterator = dataFileReader.iterator();
+      i = 0;
+      while (dataFileReader.hasNext()) {
         GenericRecord record = dataFileReader.next();
         for (int j = 0; j < columnTypes.length; j++) {
-            if (!creators[j].isSorted()) {
-                try {
-                  intArrays[j].addInt(i, creators[j].getIndex(record.get(j)));
-                } catch (Throwable ex) {
-                  StringBuilder  builder = new StringBuilder();
-                  builder.append("An error has occured: \ncapacity = " + intArrays[j].getCapacity());
-                  builder.append("\nbitSize = " + CompressedIntArray.getNumOfBits(dictionaries[j].size()));
-                  builder.append("\nbufferSize = " + CompressedIntArray.getRequiredBufferSize(intArrays[j].getCapacity(), CompressedIntArray.getNumOfBits(dictionaries[j].size())));
-                  builder.append("\ncounter = " + i);
-                  logger.error(builder.toString(), ex);
-                  throw new RuntimeException(ex);
-                }
-            } else {
-                sortedForwardIndexes[j].add(i, creators[j].getIndex(record.get(j)));
+          if (!creators[j].isSorted()) {
+            try {
+              intArrays[j].addInt(i, creators[j].getIndex(record.get(j)));
+            } catch (Throwable ex) {
+              StringBuilder builder = new StringBuilder();
+              builder.append("An error has occured: \ncapacity = " + intArrays[j].getCapacity());
+              builder.append("\nbitSize = " + CompressedIntArray.getNumOfBits(dictionaries[j].size()));
+              builder.append("\nbufferSize = "
+                  + CompressedIntArray.getRequiredBufferSize(intArrays[j].getCapacity(),
+                      CompressedIntArray.getNumOfBits(dictionaries[j].size())));
+              builder.append("\ncounter = " + i);
+              logger.error(builder.toString(), ex);
+              throw new RuntimeException(ex);
             }
-    }
+          } else {
+            sortedForwardIndexes[j].add(i, creators[j].getIndex(record.get(j)));
+          }
+        }
         i++;
-  }
-    dataFileReader.close();
-    GazelleIndexSegmentImpl indexSegmentImpl = new GazelleIndexSegmentImpl();
-    for (int j = 0; j < columnTypes.length; j++) {
-      indexSegmentImpl.getColumnTypes().put(columnNames[j], columnTypes[j]);
-      indexSegmentImpl.getDictionaries().put(columnNames[j], dictionaries[j]);
-      
-      if (!creators[j].isSorted()) {
-          ColumnMetadata metadata = creator.createMetadata(columnNames[j], dictionaries[j], columnTypes[j], count,  false);
-          GazelleForwardIndexImpl forwardIndexImpl = new GazelleForwardIndexImpl(columnNames[j], intArrays[j], dictionaries[j], metadata);
+      }
+      dataFileReader.close();
+      GazelleIndexSegmentImpl indexSegmentImpl = new GazelleIndexSegmentImpl();
+      for (int j = 0; j < columnTypes.length; j++) {
+        indexSegmentImpl.getColumnTypes().put(columnNames[j], columnTypes[j]);
+        indexSegmentImpl.getDictionaries().put(columnNames[j], dictionaries[j]);
+
+        if (!creators[j].isSorted()) {
+          ColumnMetadata metadata =
+              creator.createMetadata(columnNames[j], dictionaries[j], columnTypes[j], count, false);
+          GazelleForwardIndexImpl forwardIndexImpl =
+              new GazelleForwardIndexImpl(columnNames[j], intArrays[j], dictionaries[j], metadata);
           indexSegmentImpl.getColumnMetadataMap().put(columnNames[j], metadata);
           indexSegmentImpl.getForwardIndexes().put(columnNames[j], forwardIndexImpl);
-      } else {
+        } else {
           sortedForwardIndexes[j].seal();
           indexSegmentImpl.getForwardIndexes().put(columnNames[j], sortedForwardIndexes[j]);
-          indexSegmentImpl.getColumnMetadataMap().put(columnNames[j],  sortedForwardIndexes[j].getColumnMetadata());
+          indexSegmentImpl.getColumnMetadataMap().put(columnNames[j], sortedForwardIndexes[j].getColumnMetadata());
+        }
+
       }
-      
-  }
-    indexSegmentImpl.setLength(count);
-    return indexSegmentImpl;
-    } catch (Exception ex) {     
+      indexSegmentImpl.setLength(count);
+      return indexSegmentImpl;
+    } catch (Exception ex) {
       throw new RuntimeException(ex);
     } finally {
-        IOUtils.closeQuietly(inputStream1);
-        IOUtils.closeQuietly(inputStream2);
+      IOUtils.closeQuietly(inputStream1);
+      IOUtils.closeQuietly(inputStream2);
+    }
+  }
+
+  public static GazelleIndexSegmentImpl readFromAvroFile(File avroFile) throws IOException {
+    MetadataCreator creator = new MetadataCreator();
+    FileInputStream inputStream1 = null;
+    FileInputStream inputStream2 = null;
+    try {
+      Map<String, GazelleForwardIndexImpl> ret = new HashMap<String, GazelleForwardIndexImpl>();
+      DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
+      inputStream1 = new FileInputStream(avroFile);
+      DataFileStream<GenericRecord> dataFileReader = new DataFileStream<GenericRecord>(inputStream1, datumReader);
+
+      Schema schema = dataFileReader.getSchema();
+      if (dataFileReader.getSchema() == null) {
+        throw new IllegalStateException();
+      }
+      ColumnType[] columnTypes = new ColumnType[schema.getFields().size()];
+      String[] columnNames = new String[schema.getFields().size()];
+      int i = 0;
+      for (Field field : schema.getFields()) {
+        Type type = field.schema().getType();
+        if (type == Type.UNION) {
+          type = ((Schema) CollectionUtils.find(field.schema().getTypes(), new Predicate() {
+            @Override
+            public boolean evaluate(Object object) {
+              return ((Schema) object).getType() != Type.NULL;
+            }
+          })).getType();
+        }
+        ColumnType columnType = ColumnType.valueOf(type);
+        columnTypes[i] = columnType;
+        columnNames[i] = field.name();
+        i++;
+      }
+      DictionaryCreator[] creators = new DictionaryCreator[schema.getFields().size()];
+      for (int j = 0; j < columnTypes.length; j++) {
+        creators[j] = new DictionaryCreator();
+      }
+      int count = 0;
+      Iterator<GenericRecord> iterator = dataFileReader.iterator();
+      while (iterator.hasNext()) {
+        GenericRecord record = iterator.next();
+        for (int j = 0; j < columnTypes.length; j++) {
+
+          Object value = record.get(j);
+          if (value instanceof Utf8) {
+            value = ((Utf8) value).toString();
+          }
+          creators[j].addValue(value, columnTypes[j]);
+        }
+        /*
+         * if (count % 100000 == 0) { System.out.println("!count = " + count); }
+         */
+        count++;
+      }
+      logger.info("Created dictionaries for " + count + " elements from the avro file");
+      TermValueList[] dictionaries = new TermValueList[schema.getFields().size()];
+      for (int j = 0; j < columnTypes.length; j++) {
+        dictionaries[j] = creators[j].produceDictionary();
+      }
+      CompressedIntArray[] intArrays = new CompressedIntArray[schema.getFields().size()];
+      SortedForwardIndexImpl[] sortedForwardIndexes = new SortedForwardIndexImpl[schema.getFields().size()];
+      for (int j = 0; j < columnTypes.length; j++) {
+        if (!creators[j].isSorted()) {
+          // System.out.println(columnNames[j] + " = " +
+          // dictionaries[j].size());
+          intArrays[j] =
+              new CompressedIntArray(count, CompressedIntArray.getNumOfBits(dictionaries[j].size()), getByteBuffer(
+                  count, dictionaries[j].size()));
+        } else {
+          sortedForwardIndexes[j] =
+              new SortedForwardIndexImpl(dictionaries[j], new int[dictionaries[j].size()],
+                  new int[dictionaries[j].size()], count, creator.createMetadata(columnNames[j], dictionaries[j],
+                      columnTypes[j], count, true));
+        }
+      }
+      dataFileReader.close();
+      datumReader = new GenericDatumReader<GenericRecord>();
+      inputStream2 = new FileInputStream(avroFile);
+
+      dataFileReader = new DataFileStream<GenericRecord>(inputStream2, datumReader);
+
+      iterator = dataFileReader.iterator();
+      i = 0;
+      while (dataFileReader.hasNext()) {
+        GenericRecord record = dataFileReader.next();
+        for (int j = 0; j < columnTypes.length; j++) {
+          if (!creators[j].isSorted()) {
+            try {
+              intArrays[j].addInt(i, creators[j].getIndex(record.get(j)));
+            } catch (Throwable ex) {
+              StringBuilder builder = new StringBuilder();
+              builder.append("An error has occured: \ncapacity = " + intArrays[j].getCapacity());
+              builder.append("\nbitSize = " + CompressedIntArray.getNumOfBits(dictionaries[j].size()));
+              builder.append("\nbufferSize = "
+                  + CompressedIntArray.getRequiredBufferSize(intArrays[j].getCapacity(),
+                      CompressedIntArray.getNumOfBits(dictionaries[j].size())));
+              builder.append("\ncounter = " + i);
+              logger.error(builder.toString(), ex);
+              throw new RuntimeException(ex);
+            }
+          } else {
+            sortedForwardIndexes[j].add(i, creators[j].getIndex(record.get(j)));
+          }
+        }
+        i++;
+      }
+      dataFileReader.close();
+      GazelleIndexSegmentImpl indexSegmentImpl = new GazelleIndexSegmentImpl();
+      for (int j = 0; j < columnTypes.length; j++) {
+        indexSegmentImpl.getColumnTypes().put(columnNames[j], columnTypes[j]);
+        indexSegmentImpl.getDictionaries().put(columnNames[j], dictionaries[j]);
+
+        if (!creators[j].isSorted()) {
+          ColumnMetadata metadata =
+              creator.createMetadata(columnNames[j], dictionaries[j], columnTypes[j], count, false);
+          GazelleForwardIndexImpl forwardIndexImpl =
+              new GazelleForwardIndexImpl(columnNames[j], intArrays[j], dictionaries[j], metadata);
+          indexSegmentImpl.getColumnMetadataMap().put(columnNames[j], metadata);
+          indexSegmentImpl.getForwardIndexes().put(columnNames[j], forwardIndexImpl);
+        } else {
+          sortedForwardIndexes[j].seal();
+          indexSegmentImpl.getForwardIndexes().put(columnNames[j], sortedForwardIndexes[j]);
+          indexSegmentImpl.getColumnMetadataMap().put(columnNames[j], sortedForwardIndexes[j].getColumnMetadata());
+        }
+
+      }
+      indexSegmentImpl.setLength(count);
+      return indexSegmentImpl;
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    } finally {
+      IOUtils.closeQuietly(inputStream1);
+      IOUtils.closeQuietly(inputStream2);
     }
   }
 
--- sensei-ba/gazelle/src/main/java/com/senseidb/ba/gazelle/persist/MetadataPersistentManager.java
+++ sensei-ba/gazelle/src/main/java/com/senseidb/ba/gazelle/persist/MetadataPersistentManager.java
@@ -1,18 +1,35 @@
 package com.senseidb.ba.gazelle.persist;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import com.senseidb.ba.ColumnMetadata;
 import com.senseidb.ba.ColumnType;
 import com.senseidb.ba.gazelle.utils.GazelleUtils;
 
 public class MetadataPersistentManager {
+
+  public static void flushOnHadoop(Map<String, ColumnMetadata> metadataMap, String basePath, FileSystem fs) throws ConfigurationException, IOException {
+    Path path = new Path(basePath + "/" + GazelleUtils.METADATA_FILENAME);
+    FSDataOutputStream ds = fs.create(path);
+    PropertiesConfiguration config = new PropertiesConfiguration();
+    try {
+      for (String column : metadataMap.keySet()) {
+        metadataMap.get(column).addToConfig(config);
+      }
+    } finally {
+      config.save(ds);
+    }
+  }
 
   public static void flush(Map<String, ColumnMetadata> metadataMap, File baseDir) throws ConfigurationException {
     PropertiesConfiguration config = new PropertiesConfiguration(new File(baseDir, GazelleUtils.METADATA_FILENAME));
@@ -22,26 +39,28 @@ public class MetadataPersistentManager {
       }
     } finally {
       config.save();
+    
     }
   }
+
   public static HashMap<String, ColumnMetadata> readFromFile(PropertiesConfiguration config) {
     HashMap<String, ColumnMetadata> columnMetadataMap = new HashMap<String, ColumnMetadata>();
     Iterator columns = config.getKeys("column");
     while (columns.hasNext()) {
-       String key = (String) columns.next();
-       String columnName = key.split("\\.")[1];
-       ColumnMetadata metadata = new ColumnMetadata();
-       metadata.setStartOffset(config.getLong("column." + columnName + ".startOffset"));
-       metadata.setByteLength(config.getLong("column." + columnName + ".byteLength"));
-       metadata.setNumberOfElements(config.getInt("column." + columnName + ".numberOfElements"));
-       metadata.setNumberOfDictionaryValues(config.getInt("column." + columnName + ".numberOfDictionaryValues"));
-       metadata.setBitsPerElement(config.getInt("column." + columnName + ".bitsPerElement"));
-       metadata.setColumnType(ColumnType.valueOfStr(config.getString("column." + columnName + ".columnType")));
-       metadata.setSorted(config.getBoolean("column." + columnName + ".sorted"));
-       metadata.setName(columnName);
-       if (!columnMetadataMap.containsKey(columnName)) {
-         columnMetadataMap.put(columnName, metadata);
-       }
+      String key = (String) columns.next();
+      String columnName = key.split("\\.")[1];
+      ColumnMetadata metadata = new ColumnMetadata();
+      metadata.setStartOffset(config.getLong("column." + columnName + ".startOffset"));
+      metadata.setByteLength(config.getLong("column." + columnName + ".byteLength"));
+      metadata.setNumberOfElements(config.getInt("column." + columnName + ".numberOfElements"));
+      metadata.setNumberOfDictionaryValues(config.getInt("column." + columnName + ".numberOfDictionaryValues"));
+      metadata.setBitsPerElement(config.getInt("column." + columnName + ".bitsPerElement"));
+      metadata.setColumnType(ColumnType.valueOfStr(config.getString("column." + columnName + ".columnType")));
+      metadata.setSorted(config.getBoolean("column." + columnName + ".sorted"));
+      metadata.setName(columnName);
+      if (!columnMetadataMap.containsKey(columnName)) {
+        columnMetadataMap.put(columnName, metadata);
+      }
     }
     return columnMetadataMap;
   }
