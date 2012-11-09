9853b18ec5fb55c1305d09c167edfeadf7a7f376
Dhaval Patel <dpatel@linkedin.com>
2012-11-08 18:18:01 -0800

    logging time taken for each request inside TestUtil.search method

--- sensei-ba/ba-core/src/main/java/com/senseidb/ba/util/TestUtil.java
+++ sensei-ba/ba-core/src/main/java/com/senseidb/ba/util/TestUtil.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
 import com.senseidb.ba.gazelle.creators.SegmentCreator;
@@ -16,7 +17,8 @@ import com.senseidb.ba.gazelle.impl.GazelleIndexSegmentImpl;
 import com.senseidb.ba.gazelle.persist.SegmentPersistentManager;
 
 public class TestUtil {
-
+  private static Logger logger = Logger.getLogger(TestUtil.class);
+  
   public static GazelleIndexSegmentImpl createIndexSegment() throws URISyntaxException, Exception {
     File avroFile = new File(TestUtil.class.getClassLoader().getResource("data/sample_data.avro").toURI());
     GazelleIndexSegmentImpl indexSegmentImpl =  SegmentCreator.readFromAvroFile(avroFile);
@@ -34,6 +36,7 @@ public class TestUtil {
   }
 
   public static JSONObject search(URL url, String req) throws Exception {
+    long start = System.currentTimeMillis();
     URLConnection conn = url.openConnection();
     conn.setDoOutput(true);
     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), "UTF-8"));
@@ -42,10 +45,16 @@ public class TestUtil {
     writer.write(reqStr, 0, reqStr.length());
     writer.flush();
     BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+    
     StringBuilder sb = new StringBuilder();
     String line = null;
     while((line = reader.readLine()) != null)
       sb.append(line);
+    
+    long stop = System.currentTimeMillis();
+    
+    logger.info(" Time take for Request : " + req + " in ms:" + (stop-start));
+
     String res = sb.toString();
     // System.out.println("res: " + res);
     JSONObject ret = new JSONObject(res);
