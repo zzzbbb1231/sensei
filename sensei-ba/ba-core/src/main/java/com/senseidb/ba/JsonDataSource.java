5a0b649120ea4706345600da8cad2b1e93e9d58f
Volodymyr Zhabiuk <vzhabiuk@linkedin.com>
2012-11-11 15:48:44 -0800

    Refactored datasource iterators

--- sensei-ba/ba-core/src/main/java/com/senseidb/ba/JsonDataSource.java
+++ sensei-ba/ba-core/src/main/java/com/senseidb/ba/JsonDataSource.java
@@ -20,60 +20,36 @@ import com.senseidb.util.JSONUtil.FastJSONObject;
 
 public class JsonDataSource implements GazelleDataSource {
     private final File jsonFile;
-    private Set<JsonFileIterator> iterators = new HashSet<JsonDataSource.JsonFileIterator>();
+    private Set<JsonIterator> iterators = new HashSet<JsonIterator>();
     public JsonDataSource(File jsonFile) {
         this.jsonFile = jsonFile;
         
     }
     @Override
     public Iterator<Map<String, Object>> newIterator() {
-        JsonFileIterator ret = new JsonFileIterator(jsonFile);
+        JsonIterator ret = new JsonIterator(jsonFile);
         iterators.add(ret);
         return ret;
     }
 
     @Override
     public void closeCurrentIterators() {
-        for (JsonFileIterator iterator : iterators) {
+        for (JsonIterator iterator : iterators) {
             iterator.close();
         }
         
     }
-    public static class JsonFileIterator implements  Iterator<Map<String, Object>> {
-        private LineIterator lineIterator;
-        private Field mapField;
-        Map<String, Object> _next;
-        public JsonFileIterator(File jsonFile)  {
-            try { 
-            lineIterator = FileUtils.lineIterator(jsonFile);
-                  mapField = JSONObject.class.getDeclaredField("map");
-                 mapField.setAccessible(true);
-             } catch (Exception e) {
-                 throw new RuntimeException(e);
-             }
-        }
-        
-        public Map<String, Object> _next() {
-            while(lineIterator.hasNext()) {
-                String line = lineIterator.next();
-                if (line != null && line.contains("{")) {
-                    try {
-                        FastJSONObject fastJSONObject = new JSONUtil.FastJSONObject(line);
-                        JSONObject innerJSONObject = fastJSONObject.getInnerJSONObject();
-                        Map map = (Map) mapField.get(innerJSONObject);
-                        for (Object key : map.keySet()) {
-                            Object value = map.get(key);
-                            if (value instanceof JSONArray) {
-                                map.put(key, transform((JSONArray) value)); 
-                            }
-                        }
-                        return map;
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
+    public static class JsonIterator extends AbstractIterator {
+        public JsonIterator(File jsonFile)  {
+            super(jsonFile);
+            Field mapField;
+            try {
+                mapField = JSONObject.class.getDeclaredField("map");
+                mapField.setAccessible(true);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-            return null;
+            
         }
         private Object transform(JSONArray value) throws JSONException {
             Object[] ret = new Object[value.length()];
@@ -82,33 +58,26 @@ public class JsonDataSource implements GazelleDataSource {
             }
             return ret;
         }
-
-        @Override
-        public boolean hasNext() {
-            if (_next == null) {
-                _next = _next();
+        protected Map<String, Object> processLine(String line) {
+            if (line != null && line.contains("{")) {
+                try {
+                    FastJSONObject fastJSONObject = new JSONUtil.FastJSONObject(line);
+                    JSONObject innerJSONObject = fastJSONObject.getInnerJSONObject();
+                    Field mapField = JSONObject.class.getDeclaredField("map");
+                    mapField.setAccessible(true);
+                    Map map = (Map) mapField.get(innerJSONObject);
+                    for (Object key : map.keySet()) {
+                        Object value = map.get(key);
+                        if (value instanceof JSONArray) {
+                            map.put(key, transform((JSONArray) value)); 
+                        }
+                    }
+                    return map;
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
-            return _next != null;
-        }
-
-        @Override
-        public Map<String, Object> next() {
-            Map<String, Object> ret = null;
-            if (_next == null) {
-                _next = _next();
-            }
-            ret = _next;
-            _next = null;
-            return ret;
-        }
-
-        @Override
-        public void remove() {
-            // TODO Auto-generated method stub
-            
-        }
-        public void close() {
-            lineIterator.close();
+            return null;
         }
     }
 }
